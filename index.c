50e5d2c714defcc9d8ed99aa2e914f419747f0d6
Manuayu <44335325+Manuayu@users.noreply.github.com>
2019-01-24 13:37:51 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int n,a[500],i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        printf("%d %d\n",a[i],i);
+    }
+    
+    
+    
+}
